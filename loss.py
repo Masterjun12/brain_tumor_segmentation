3732ec03aba3cd12cb01bf6c3c1a90a0638304e1
Master.Jun <82173315+Masterjun12@users.noreply.github.com>
2024-06-12 14:40:55 +0900

    Update loss.py
    Signed-off-by: Master.Jun <82173315+Masterjun12@users.noreply.github.com>

--- loss.py
+++ loss.py
@@ -15,6 +15,39 @@ def bce_dice_loss(y_true, y_pred):
     return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
 
 # ELL
-def ell_loss(y_true, y_pred, alpha=0.2):
-    return alpha * binary_crossentropy(y_true, y_pred) + (1 - alpha) * dice_loss(y_true, y_pred)
+
+#Focal
+def focal_loss(y_true, y_pred, alpha=0.25, gamma=2.0):
+    """
+    Focal Loss function.
+    :param y_true: True labels
+    :param y_pred: Predicted labels
+    :param alpha: Alpha value for balancing class frequencies
+    :param gamma: Gamma value for adjusting the rate of focus on hard examples
+    :return: Focal Loss
+    """
+    # Compute Binary Crossentropy
+    bce = binary_crossentropy(y_true, y_pred)
+    
+    # Compute Focal Loss
+    p_t = (y_true * y_pred) + ((1 - y_true) * (1 - y_pred))
+    alpha_factor = y_true * alpha + (1 - y_true) * (1 - alpha)
+    modulating_factor = K.pow((1.0 - p_t), gamma)
+    focal_loss = alpha_factor * modulating_factor * bce
+    
+    return focal_loss
+#DiceFocal
+def bce_dice_focal_loss(y_true, y_pred, alpha=0.25, gamma=2.0):
+    """
+    Combined Binary Crossentropy, Dice, and Focal Loss function.
+    :param y_true: True labels
+    :param y_pred: Predicted labels
+    :param alpha: Alpha value for balancing class frequencies in Focal Loss
+    :param gamma: Gamma value for adjusting the rate of focus on hard examples in Focal Loss
+    :return: Combined loss
+    """
+    bce_dice = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+    focal = focal_loss(y_true, y_pred, alpha=alpha, gamma=gamma)
+    return bce_dice + focal
+
 
