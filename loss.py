0b2804288e24174d02d222d5abba3b71e1118d01
Master.Jun <82173315+Masterjun12@users.noreply.github.com>
2024-06-12 18:30:56 +0900

    Update loss.py
    Signed-off-by: Master.Jun <82173315+Masterjun12@users.noreply.github.com>

--- loss.py
+++ loss.py
@@ -17,22 +17,24 @@ def bce_dice_loss(y_true, y_pred):
 # alphabcedice
 def alphabcedice(y_true, y_pred, alpha=0.2):
     return alpha * binary_crossentropy(y_true, y_pred) + (1 - alpha) * dice_loss(y_true, y_pred)
-    
-#Focal
-def focal_loss(y_true, y_pred, gamma=2.0, alpha=0.25):
-    y_true = tf.convert_to_tensor(y_true, dtype=tf.float32)
-    y_pred = tf.convert_to_tensor(y_pred, dtype=tf.float32)
 
-    # Focal Loss 계산
-    epsilon = K.epsilon()
-    y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
-    y_true = tf.cast(y_true, tf.float32)
-    alpha_t = y_true * alpha + (K.ones_like(y_true) - y_true) * (1 - alpha)
-    p_t = y_true * y_pred + (K.ones_like(y_true) - y_true) * (1 - y_pred)
-    fl = - alpha_t * K.pow((K.ones_like(y_true) - p_t), gamma) * K.log(p_t)
-    
-    return K.sum(fl, axis=-1)
+#focal
+def focal_loss(gamma=2., alpha=0.25):
+    def focal_loss_fixed(y_true, y_pred):
+        y_true = tf.convert_to_tensor(y_true, dtype=tf.float32)
+        y_pred = tf.convert_to_tensor(y_pred, dtype=tf.float32)
 
+        epsilon = K.epsilon()
+        y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+        y_true = tf.cast(y_true, tf.float32)
+        
+        alpha_t = y_true * alpha + (1 - y_true) * (1 - alpha)
+        p_t = y_true * y_pred + (1 - y_true) * (1 - y_pred)
+        fl = - alpha_t * K.pow((1 - p_t), gamma) * K.log(p_t)
+        
+        return tf.reduce_mean(tf.reduce_sum(fl, axis=-1))  # 각 클래스에 대해 손실을 계산한 후 축소
+    return focal_loss_fixed
+        
 
 def focal_dice_loss(y_true, y_pred, alpha=0.25, gamma=2.0):
     return focal_loss(y_true, y_pred, gamma, alpha) + dice_loss(y_true, y_pred)
