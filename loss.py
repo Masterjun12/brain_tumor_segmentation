286c5fd5bafe03b25ea3bc0c5855fa7182a37af1
Master.Jun <82173315+Masterjun12@users.noreply.github.com>
2024-07-02 17:00:18 +0900

    Update loss.py
    Signed-off-by: Master.Jun <82173315+Masterjun12@users.noreply.github.com>

--- loss.py
+++ loss.py
@@ -2,12 +2,14 @@ from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras import backend as K
 import tensorflow as tf
 
-def dice_coef(y_true, y_pred, smooth=100):        
+def dice_loss(y_true, y_pred):
+    smooth = 1e-6
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    dice = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return dice
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
 
 # Binary Crossentropy Dice Loss
 def bce_dice_loss(y_true, y_pred):
