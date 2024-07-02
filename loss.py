093d8cb31b667fc82dbed212413865f528f2aaef
Master.Jun <82173315+Masterjun12@users.noreply.github.com>
2024-07-02 15:24:31 +0900

    Update loss.py
    Signed-off-by: Master.Jun <82173315+Masterjun12@users.noreply.github.com>

--- loss.py
+++ loss.py
@@ -2,13 +2,12 @@ from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras import backend as K
 import tensorflow as tf
 
-def dice_loss(y_true, y_pred):
-    smooth = 1e-6
+def dice_coef(y_true, y_pred, smooth=100):        
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
-    intersection = y_true_f * y_pred_f
-    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return 1. - score
+    intersection = K.sum(y_true_f * y_pred_f)
+    dice = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return dice
 
 # Binary Crossentropy Dice Loss
 def bce_dice_loss(y_true, y_pred):
@@ -19,7 +18,7 @@ def alphabcedice(y_true, y_pred, alpha=0.2):
     return alpha * binary_crossentropy(y_true, y_pred) + (1 - alpha) * dice_loss(y_true, y_pred)
 
 #focal
-def focal_loss(gamma=2., alpha=0.25):
+def focal_loss(gamma=2.5, alpha=0.5):
     def focal_loss_fixed(y_true, y_pred):
         y_true = tf.convert_to_tensor(y_true, dtype=tf.float32)
         y_pred = tf.convert_to_tensor(y_pred, dtype=tf.float32)
